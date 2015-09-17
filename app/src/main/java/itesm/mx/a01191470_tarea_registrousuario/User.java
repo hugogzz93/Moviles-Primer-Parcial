7620b07c3d24e0666f6fcd96d28846d956170de2
Hugo Gonzalez <hugo@Hugos-MacBook-Pro.local>
2015-09-17 14:47:45 -0500

    Log in functional

--- app/src/main/java/itesm/mx/a01191470_tarea_registrousuario/User.java
+++ app/src/main/java/itesm/mx/a01191470_tarea_registrousuario/User.java
@@ -4,14 +4,12 @@ package itesm.mx.a01191470_tarea_registrousuario;
  * Created by hugo on 9/8/15.
  */
 public class User {
-    public String fullName;
     public String email;
     public String password;
 
-    public User(String nfullName,String nemail,String npassword) {
-        fullName = nfullName;
-        email = nemail;
-        password = npassword;
+    public User(String email,String password) {
+        this.email = email;
+        this.password = password;
     }
 
 
--- app/src/main/java/itesm/mx/a01191470_tarea_registrousuario/login.java
+++ app/src/main/java/itesm/mx/a01191470_tarea_registrousuario/login.java
@@ -8,13 +8,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class login extends AppCompatActivity {
+
+    ArrayList<User> userList = new ArrayList<User>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        String email = "usuario1@gmail.com";
+        String password = "usuario1";
+        User newUser = new User( email, password);
+        userList.add(newUser);
+
+        email = "usuario2@gmail.com";
+        password = "usuario2";
+        newUser = new User( email, password);
+        userList.add(newUser);
 
         final EditText emailET = (EditText)findViewById(R.id.emailLoginET);
         final EditText passwordET = (EditText)findViewById(R.id.passwordLoginET);
@@ -25,13 +41,19 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
                 String email = emailET.getText().toString();
                 String password = passwordET.getText().toString();
-                Intent loginUser = new Intent();
-                Bundle info = new Bundle();
-                info.putString("email", email);
-                info.putString("password", password );
-                loginUser.putExtras(info);
-                setResult(RESULT_OK, loginUser);
-                finish();
+//                Intent loginUser = new Intent();
+//                Bundle info = new Bundle();
+//                info.putString("email", email);
+//                info.putString("password", password );
+//                loginUser.putExtras(info);
+//                setResult(RESULT_OK, loginUser);
+//                finish();
+                User user = new User(email, password);
+                boolean found = findUser(user);
+                if(found) {
+                    Intent intent = new Intent(login.this, lista_articulos.class);
+                    startActivity(intent);
+                }
             }
         };
 
@@ -58,5 +80,27 @@ public class login extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean findUser(User user) {
+
+        boolean found = false;
+
+        for (User x : userList) {
+            if (x.email.equals(user.email)) {
+                if (x.password.equals(user.password)) {
+//                    Intent userProfile = new Intent(login.this, Perfil.class);
+//                    Bundle userInfo = new Bundle();
+//                    userProfile.putExtras(userInfo);
+//                    startActivity(userProfile);
+                    found = true;
+                }
+            }
+        }
+        if(!found) {
+            Toast.makeText(getApplicationContext(), "Correo o Contraseña Incorrecta", Toast.LENGTH_SHORT).show();
+        }
+
+        return found;
     }
 }
